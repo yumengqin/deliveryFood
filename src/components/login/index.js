55b2a1ae2ce28379511a872003785fed7e472720
yumeng <qinyumeng@ciphertrading.com>
2017-04-11 09:53:27 +0800

    登录组建

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
-
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 require('./index.less');
+
+const FormItem = Form.Item;
+const createForm = Form.create;
 
 class LoginPage extends React.Component {
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className="login">
-        123
+        <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(
+                <Checkbox>Remember me</Checkbox>
+              )}
+              <a className="login-form-forgot" href="">Forgot password</a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </FormItem>
+        </Form>
       </div>
     );
   }
 }
 
-export default LoginPage
+LoginPage.propTypes = {
+  form: PropTypes.shape(),
+};
+
+export default connect()(createForm()(LoginPage))
