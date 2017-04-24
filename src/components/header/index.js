765f12dcb74e8b0f32d953c9b8d59d86e4d42c22
yumeng <qinyumeng@ciphertrading.com>
2017-04-24 23:47:21 +0800

    buyer index

---
+++ src/components/header/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router';
+
+require('./index.less');
+
+const menu = [
+  { title: '首页', url: '/indexBuyer' },
+  { title: '我的订单', url: '/allOrder' },
+  { title: '我的收藏', url: '/allLike' },
+]
+
+class Header extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+  render() {
+    return (
+      <div className="buyer-head">
+        <ul>
+          {
+            menu.map((item, index) => (
+              <li key={index}>
+                <Link to={item.url}>{item.title}</Link>
+              </li>
+            ))
+          }
+          <li key="login-signup">
+            <Link to="/login">登录／注册</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/pages/index/buyer.js
+++ src/pages/index/buyer.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { message_update, guest_update, nickname_get } from '../../action'
 import { hashHistory } from 'react-router'
+import Home from '../../components/header'
 
 class IndexPage extends React.Component {
   render() {
     return (
-      <div>进入页面</div>
+      <div>
+        <Home />
+      </div>
     );
   }
 }
